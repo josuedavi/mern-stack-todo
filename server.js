4157dfe4124a98cd159d5aa977fab8824adc8d7d
josuedavi <ijosuedavi@gmail.com>
2022-02-25 14:33:38 -0500

    update server.js

--- server.js
+++ server.js
@@ -16,6 +16,18 @@ const route = require('./controllers/todo');
 // connect database
 connectDB();
 
+// ... other imports 
+const path = require("path")
+
+// ... other app.use middleware 
+app.use(express.static(path.join(__dirname, "client", "build")))
+
+// ...
+// Right before your app.listen(), add this:
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
+
 // cors
 app.use(cors({ origin: true, credentials: true }));
 
